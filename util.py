560be8bae277898985a6cc7275f81272884c9635
YJThhh <74174784+YJThhh@users.noreply.github.com>
2021-12-05 17:08:08 +0800

    init

---
+++ util.py
@@ -0,0 +1,12 @@
+import numpy as np
+import torch
+
+
+def fix(env):
+    seed = 543
+    env.seed(seed)
+    env.action_space.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
